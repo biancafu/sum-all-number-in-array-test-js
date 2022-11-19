729549b810e3f2776bc51ef64c6958c90b31fb9d
Bianca Fu <0509biancafu@gmail.com>
2022-11-19 00:14:25 +0000

    W1D5 stretch recursion to sum all items in array

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+  for (const element of array) {
+    if (!Array.isArray(element)) { 
+      sum += element;
+    } else {
+      //recursive case
+      sum += sumItems(element);
+    }
+  }
+  return sum;
 }
 
 module.exports = sumItems;
